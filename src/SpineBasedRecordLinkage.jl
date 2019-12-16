4ab34486c2e1a1566b2a410da59276107b2d5a3b
Jock Lawrie (DHHS) <Jock.Lawrie@dhhs.vic.gov.au>
2019-12-16 14:34:14 +1100

    Cleaned up the SpineBasedRecordLinkage.jl file.

--- src/SpineBasedRecordLinkage.jl
+++ src/SpineBasedRecordLinkage.jl
@@ -2,28 +2,15 @@ module SpineBasedRecordLinkage
 
 export run_linkage, summarise_linkage_run, compare_linkage_runs
 
-#=
-TODO:
-3. testset5 (to be named testset3) compares the 2 linkage runs.
-
-Issue: Merging a row into the spine may alter the primary key and therefore the spineid.
-       Update the spineid and all links involving this row.
-       This means doing everything in memory and maintaining a lookup: spineid => [rowindices...]
-=#
-
-# Functions that aren't exported
 include("unexported/distances.jl")     # Independent
 include("unexported/TableIndexes.jl")  # Independent
 include("unexported/config.jl")        # Depends on distances
+include("runlinkage.jl")               # Exports: run_linkage
+include("reporting.jl")                # Exports: summarise_linkage_run, compare_linkage_runs
 
 using .distances
 using .TableIndexes
 using .config
-
-# Exported functions
-include("runlinkage.jl")
-include("reporting.jl")
-
 using .runlinkage
 using .reporting
 
